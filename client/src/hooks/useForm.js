a459b3386ee3191b674e7849b81fd1c0082d481b
Karla Cervantes <cervantes.karla14@gmail.com>
2021-01-15 11:14:06 -0600

    custom hooks created

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,21 @@
 // write your custom hook here to control your checkout form
+import e from 'cors';
+import { useState } from 'react';
+
+const useForm = (initialValues) => {
+    const [values, setValues] = useState(initialValues);
+
+    const handleChanges = e => {
+        setValues({
+            ...values,
+        [e.target.name]: e.target.value
+        });
+    };
+    
+    // const clearForm = e => {
+    //     e.preventDefault();
+    //     setValues(initialValues);
+    // };
+return ([values, handleChanges]);
+};
+export default useForm;
